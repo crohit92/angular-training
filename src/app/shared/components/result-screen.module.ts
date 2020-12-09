30ee75c178cc275a75c56a82602d73d0ed720e26
Rohit Chopra <crohit92@gmail.com>
2020-12-09 17:57:47 +0530

    Remove old code

--- src/app/shared/components/result-screen.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { ResultScreenComponent } from "./result-screen.component";
-@NgModule({
-  declarations: [ResultScreenComponent],
-  imports: [FormsModule],
-  exports: [ResultScreenComponent],
-})
-export class ResultScreenModule {}
