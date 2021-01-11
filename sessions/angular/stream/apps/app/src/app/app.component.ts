5f277bd6e96deaea50e5e6bb7295320d64bb4288
Rohit Chopra <crohit92@gmail.com>
2021-01-11 14:00:46 +0530

    Create NX Workspace

---
+++ sessions/angular/stream/apps/app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stream-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title: string;
+}
+// npm i -g nx
---
+++ sessions/angular/stream/apps/app/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [AppComponent, DashboardComponent, LoginComponent],
+  imports: [BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
