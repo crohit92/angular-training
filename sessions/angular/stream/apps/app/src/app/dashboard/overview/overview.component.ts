1e71e419d34cdefb7d96c348f4741c475ae2b2b4
Rohit Chopra <crohit92@gmail.com>
2021-01-20 16:00:11 +0530

    WIP overview screen

--- sessions/angular/stream/apps/app/src/app/dashboard/overview/overview.component.ts
+++ sessions/angular/stream/apps/app/src/app/dashboard/overview/overview.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
 @Component({
   selector: 'stream-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss']
 })
-export class OverviewComponent implements OnInit {
+export class OverviewComponent implements OnInit, OnDestroy {
+  workshop: any;
+  destroy$ = new Subject();
+  constructor(private http: HttpClient,
+    private route: ActivatedRoute,
+    private sanitizer: DomSanitizer) { }
 
-  constructor() { }
+  ngOnDestroy(): void {
+
+    if (!this.destroy$.closed) {
+      this.destroy$.next({});
+      this.destroy$.complete();
+      this.destroy$.unsubscribe();
+    }
+  }
 
   ngOnInit(): void {
+    this.route.paramMap.pipe(takeUntil(this.destroy$)).subscribe(params => {
+      this.http.get('./assets/workshops/workshops.json').pipe(
+        takeUntil(this.destroy$),
+        map((workshops: any[]) => {
+          return workshops.find(w => `${w.id}` === params.get('id'));
+        }))
+        .subscribe((workshop => {
+          // document.getElementById('videoContainer').innerHTML = workshop.videoEmbed;
+          workshop.videoEmbed = this.sanitizer.bypassSecurityTrustHtml(workshop.videoEmbed);
+          this.workshop = workshop;
+        }));
+
+    })
+
+    // const s = new Subject();
+
+    // const ob = new Observable((subscriber) => {
+    //   subscriber.next(10);
+    //   subscriber.next(11);
+    //   subscriber.next(12);
+    //   subscriber.next(13);
+    //   // subscriber.complete();
+    //   // subscriber.unsubscribe();
+    // });
+
+    // ob.subscribe((value) => { console.log(value) });
   }
 
 }
